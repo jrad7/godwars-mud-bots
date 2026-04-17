c5a2fc114069f1d1839fba3f734e42d62fd5e367
Jared Wright <jared.w.wright@gmail.com>
2026-04-17 18:36:49 -0500

    fix long nav paths

--- src/bot_ai.c
+++ src/bot_ai.c
@@ -225,7 +225,7 @@ void bot_cmd( CHAR_DATA *ch, const char *cmd )
         if ( bot->cmd_history_count < 10 )
             bot->cmd_history_count++;
 
-        if ( bot->cmd_history_count >= 10 && !bot->spell_training )
+        if ( bot->cmd_history_count >= 10 && !bot->spell_training && bot->nav_n == 0 )
         {
             bool stuck = TRUE;
             int i;
